e8edf4fd0f7df69ce46a086285ea6748f00e9691
Tommy Armstrong <tommy.armstrong@macbook.localhost>
2024-04-10 12:36:13 +0100

    NCD-1 Updated code so that grid is generated thropugh loops not hardcoded

--- src/App.js
+++ src/App.js
@@ -33,21 +33,20 @@ function Board({ xIsNext, squares, onPlay }) {
   return (
     <>
       <div className="status">{status}</div>
-      <div className="board-row">
-        <Square value={squares[0]} onSquareClick={ () => handleClick(0)}/>
-        <Square value={squares[1]} onSquareClick={ () => handleClick(1)}/>
-        <Square value={squares[2]} onSquareClick={ () => handleClick(2)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[3]} onSquareClick={ () => handleClick(3)}/>
-        <Square value={squares[4]} onSquareClick={ () => handleClick(4)}/>
-        <Square value={squares[5]} onSquareClick={ () => handleClick(5)}/>
-      </div>
-      <div className="board-row">
-        <Square value={squares[6]} onSquareClick={ () => handleClick(6)}/>
-        <Square value={squares[7]} onSquareClick={ () => handleClick(7)}/>
-        <Square value={squares[8]} onSquareClick={ () => handleClick(8)}/>
-      </div>
+      {Array(3).fill(null).map((_, rowIndex) => (
+        <div className="board-row" key={rowIndex}>
+          {Array(3).fill(null).map((_, colIndex) => {
+            const squareIndex = rowIndex * 3 + colIndex;
+            return (
+              <Square 
+                key={squareIndex}
+                value={squares[squareIndex]}
+                onSquareClick={() => handleClick(squareIndex)}
+              />
+            );
+          })}
+        </div>
+      ))}
     </>
   );
 }
